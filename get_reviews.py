92fde59468312ae8f4dc8ec11e5feb6c49f43beb
Roman <romnklymko@gmail.com>
2019-10-15 17:03:36 +0200

    txt
    write reviews in txt

--- get_reviews.py
+++ get_reviews.py
@@ -5,3 +5,8 @@ print(df.columns.values)
 reviews = df['Text'].dropna().tolist()
 print(reviews)
 print(len(reviews))
+
+with open('reviews.txt', 'w') as f:
+    for review in reviews:
+        f.write("%s\n" % review)
+print("Done")
