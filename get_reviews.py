ad3bb3ba2381137bdea682185ebf9310e4c67b65
Roman Klymko <romnklymko@gmail.com>
2019-10-16 20:28:09 +0200

    read & clean text

--- get_reviews.py
+++ get_reviews.py
@@ -1,7 +1,17 @@
 import pandas as pd
 
+# read
 df = pd.read_csv('amazon-fine-food-reviews/Reviews.csv')
 print(df.columns.values)
-reviews = df['Text'].dropna().tolist()
-print(reviews)
+
+# clean
+df = df[df.Score > 2]
+df_reviews = df['Text'].dropna().drop_duplicates()
+reviews = df_reviews.tolist()
+#print(reviews)
 print(len(reviews))
+
+with open('reviews.txt', 'w') as f:
+    for review in reviews:
+        f.write("%s\n" % review)
+print("Done")
