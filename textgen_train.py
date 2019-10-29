1f09905f75ca76456c67b1315788d83657faba6e
Roman Klymko <romnklymko@gmail.com>
2019-10-29 19:41:12 +0100

    prepare dataset

--- textgen_train.py
+++ textgen_train.py
@@ -19,5 +19,39 @@ def main():
     text_as_int = np.array([char2idx[c] for c in text])
     print('{} ---- characters mapped to int ---- > {}'.format(repr(text[:13]), text_as_int[:13]))
 
+    # The maximum length sentence we want for a single input in characters
+    seq_length = 100
+    examples_per_epoch = len(text) // (seq_length + 1)
+
+    # Create training examples / targets
+    char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+
+    for i in char_dataset.take(5):
+        print(idx2char[i.numpy()])
+
+    sequences = char_dataset.batch(seq_length + 1, drop_remainder=True)
+
+    for item in sequences.take(5):
+        print(repr(''.join(idx2char[item.numpy()])))
+
+    def split_input_target(chunk):
+        input_text = chunk[:-1]
+        target_text = chunk[1:]
+        return input_text, target_text
+
+    dataset = sequences.map(split_input_target)
+
+    for input_example, target_example in dataset.take(1):
+        print('Input data: ', repr(''.join(idx2char[input_example.numpy()])))
+        print('Target data:', repr(''.join(idx2char[target_example.numpy()])))
+
+    BATCH_SIZE = 64
+    BUFFER_SIZE = 10000
+
+    dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+
+    print(dataset)
+
+
 if __name__ == '__main__':
     main()
