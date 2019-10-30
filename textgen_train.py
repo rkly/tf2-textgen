96c6a9d4afb9233dd59c7193f515c84481057828
Roman Klymko <romnklymko@gmail.com>
2019-10-30 21:17:31 +0100

    test dataset & model

--- textgen_train.py
+++ textgen_train.py
@@ -52,6 +52,44 @@ def main():
 
     print(dataset)
 
+    # Length of the vocabulary in chars
+    vocab_size = len(vocab)
+    # The embedding dimension
+    embedding_dim = 256
+    # Number of RNN units
+    rnn_units = 1024
+
+    def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+        model = tf.keras.Sequential([
+            tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                                      batch_input_shape=[batch_size, None]),
+            tf.keras.layers.GRU(rnn_units,
+                                return_sequences=True,
+                                stateful=True,
+                                recurrent_initializer='glorot_uniform'),
+            tf.keras.layers.Dense(vocab_size)
+        ])
+        return model
+
+    model = build_model(
+        vocab_size=len(vocab),
+        embedding_dim=embedding_dim,
+        rnn_units=rnn_units,
+        batch_size=BATCH_SIZE)
+
+    for input_example_batch, target_example_batch in dataset.take(1):
+        example_batch_predictions = model(input_example_batch)
+        print(example_batch_predictions.shape, "# (batch_size, sequence_length, vocab_size)")
+
+    model.summary()
+
+    sampled_indices = tf.random.categorical(example_batch_predictions[0], num_samples=1)
+    sampled_indices = tf.squeeze(sampled_indices, axis=-1).numpy()
+    print(sampled_indices)
+
+    print("Input: \n", repr("".join(idx2char[input_example_batch[0]])))
+    print()
+    print("Next Char Predictions: \n", repr("".join(idx2char[sampled_indices])))
 
 if __name__ == '__main__':
     main()
